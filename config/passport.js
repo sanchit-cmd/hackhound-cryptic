7238538fb78520f2143630de1a455bec93907d49
sanchit-cmd <sanchit.nigam2005@gmail.com>
2025-02-24 15:26:34 +0530

    package changes

--- config/passport.js
+++ config/passport.js
@@ -1,46 +1,47 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/userModel');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/userModel");
 
 passport.serializeUser((user, done) => {
-	done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-	try {
-		const user = await User.findById(id);
-		done(null, user);
-	} catch (err) {
-		done(err);
-	}
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
 });
 
 passport.use(
-	new GoogleStrategy(
-		{
-			clientID: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: '/api/v1/users/auth/google/callback',
-			userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
-		},
-		async (accessToken, refreshToken, profile, done) => {
-			try {
-				// Check if user already exists
-				let user = await User.findOne({ googleId: profile.id });
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL:
+        "https://pony-sharing-kite.ngrok-free.app/api/v1/users/auth/google/callback",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // Check if user already exists
+        let user = await User.findOne({ googleId: profile.id });
 
-				if (!user) {
-					// Create new user
-					user = await User.create({
-						username: profile.displayName,
-						googleId: profile.id,
-						email: profile.emails[0].value,
-					});
-				}
+        if (!user) {
+          // Create new user
+          user = await User.create({
+            username: profile.displayName,
+            googleId: profile.id,
+            email: profile.emails[0].value,
+          });
+        }
 
-				done(null, user);
-			} catch (err) {
-				done(err);
-			}
-		}
-	)
+        done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    },
+  ),
 );
